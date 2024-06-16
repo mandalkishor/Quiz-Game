2c66dc6fd04f73294c9587d13af033f62f6235a3
KISHOR KUMAR MANDAL <143748511+mandalkishor@users.noreply.github.com>
2024-06-16 17:02:47 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,12 +1,35 @@
-questions=("KKKKK","MMMMMM","MMMMMM","MMMMMM","MMMMMM")
+questions=("1. What does HTML stand for?",
+           "2.What does CSS stand for?",
+           "3.Which of the following is the correct way to use the standard namespace in C++?",
+           "4.Which of the following is not a part of the string class",
+           "5.Which of the following classes would allow random numbers to be generated?",
+           "6.An applet doesn't have a _______ method",
+           "7.What year was Java created?",
+           "8.What company helped create Java?",
+           "9.What is the name of the program we use to develop out Java programs?",
+           "10.Which of the following are not one of the five main parts of a Java program?")
 
-options=(("A. ","B. ","C. ","D"),
-         ("A. ","B. ","C. ","D"),
-         ("A. ","B. ","C. ","D"),
-         ("A. ","B. ","C. ","D"),
-         ("A. ","B. ","C. ","D"))
+options=(("A.Hyper Trainer Marking Language ",
+          "B.Hyper Text Marketing Language ",
+          "C.Hyper Text Markup Language ",
+          "D.Hyper Text Markup Leveler"),
+         ("A.Computer Style Sheets ",
+          "B.Creative Style Sheets ",
+          "C.Colorful Style Sheets ",
+          "D.Cascading Style Sheets"),
+         ("A.Using namespace std; ",
+          "B.Using namespace standard; ",
+          "C.Using standard namespace; ",
+          "D.Standard namespace used;"),
+         ("A.CompareTo ","B.Length ","C.Concat ","D.ToString"),
+         ("A.Math ","B.Random ","C.String ","D.Keyboard"),
+         ("A.Main","B.Secondary","C.Primary","D.System"),
+         ("A.1996","B.1994","C.1995","D.1999"),
+         ("A.Microsoft","B.Sun Microsystem","C.Norton's","D.Dell"),
+         ("A.Java Creator","B.JCreator","C.Creator Java","D.Visual Java 6.0"),
+         ("A.Main Method","B.Class Header","C.Class Body","D.Main Body"))
 
-answers=("C","D","A","B")
+answers=("C","D","A","D","B","A","C","B","B","D")
 guesses=[]
 
 score=0
@@ -17,8 +40,8 @@ for question in questions:
     print("__________________")
     print(question)
 
-for option in options[question_num]:
-    print(option)
+    for option in options[question_num]:
+        print(option)
 
 
     guess=input("Enter(A, B, C, D):").upper()
@@ -47,3 +70,4 @@ print()
 
 score=int(score/len(questions) * 100)
 print(f"your score is:{score}%")
+  
